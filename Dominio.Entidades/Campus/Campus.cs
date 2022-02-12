b199bdcc49413a80a82da33ac30e2831c16ef025
Wilderysb <wilderysb@hotmail.com>
2022-02-12 16:38:20 +0000

    80

---
+++ Dominio.Entidades/Campus/Campus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidades.Campus
+{
+    public class Campus
+    {
+        [Key]
+        public int Id { get; set; }
+        public string NombreCampus { get; set; }
+    }
+}
---
+++ Dominio.Entidades/SubTipoEvento/SubTipoEvento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidades
+{
+    public class SubTipoEvento
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string DesSubTipoEvento { get; set; }
+
+        [ForeignKey("IdTipoEvento")]
+        public virtual TipoEvento TipoEventos { get; set; }
+    }
+}
